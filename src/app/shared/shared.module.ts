9948c65389048e5c2ffbe7004dcc3253daa4570b
Taylor Buckner <tbuckner@theappraisallane.com>
2018-05-28 11:29:20 -0500

    Refactored Shared Module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,42 @@
 import { NgModule } from "@angular/core";
 import { ShowRequiredErrorDirective } from "./show-required-error.directive";
+import { ProjectComponent } from "./components/project/project.component";
+import { DropdownComponent } from "./components/dropdown/dropdown.component";
+import { InputDefaultComponent } from "./components/input-default/input-default.component";
+import { AtlassianCredsComponent } from "../core/components/atlassian-creds/atlassian-creds.component";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatRadioModule, MatExpansionModule, MatIconModule } from "@angular/material";
 
 @NgModule({
   declarations: [
     ShowRequiredErrorDirective,
+    ProjectComponent,
+    DropdownComponent,
+    InputDefaultComponent,
+    AtlassianCredsComponent,
+  ],
+  imports: [
+    // BrowserModule,
+    BrowserAnimationsModule,
+    // FormsModule,
+    ReactiveFormsModule,
+    // MatButtonModule,
+    // MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    // MatExpansionModule,
+    // MatIconModule
   ],
   exports: [
     ShowRequiredErrorDirective,
+    ProjectComponent,
+    DropdownComponent,
+    InputDefaultComponent,
+    AtlassianCredsComponent,
   ]
 })
 export class SharedModule{}
